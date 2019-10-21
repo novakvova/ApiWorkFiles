0b3c096f1da0cc9ff95da7caf07ad86b89804028
novakvova <novakvova@gmail.com>
2019-10-21 13:23:13 +0300

    Begin work images

---
+++ WebApiFiles/BLL/Sevices/ImageWorker.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiFiles.BLL.Interfaces;
+
+namespace WebApiFiles.BLL.Sevices
+{
+    public class ImageWorker : IImageWorker
+    {
+        private readonly IConfiguration _configuration;
+
+        public ImageWorker(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+        public string SaveImage(string base64)
+        {
+            string folderName = _configuration.GetValue<string>("ImagesPath");
+
+            throw new NotImplementedException();
+        }
+    }
+}
--- WebApiFiles/Startup.cs
+++ WebApiFiles/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using WebApiFiles.BLL.Interfaces;
+using WebApiFiles.BLL.Sevices;
 
 namespace WebApiFiles
 {
@@ -24,6 +26,13 @@ namespace WebApiFiles
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //при каждом обращении к сервису создается новый объект сервиса.
+            //В течение одного запроса может быть несколько обращений к сервису, 
+            //соответственно при каждом обращении будет создаваться новый объект.
+            //Подобная модель жизненного цикла наиболее подходит для легковесных сервисов, 
+            //которые не хранят данных о состоянии
+            services.AddTransient<IImageWorker, ImageWorker>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
