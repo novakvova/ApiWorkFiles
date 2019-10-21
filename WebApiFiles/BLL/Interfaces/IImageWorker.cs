6230a1194cae3e39e20a3d7182dc68ad599fe55d
novakvova <novakvova@gmail.com>
2019-10-21 12:30:19 +0300

    Begin work Images by WebApi

---
+++ WebApiFiles/BLL/Interfaces/IImageWorker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiFiles.BLL.Interfaces
+{
+    public interface IImageWorker
+    {
+        /// <summary>
+        /// Додати фото на сервер
+        /// </summary>
+        /// <param name="base64">Фото у форматі base64</param>
+        /// <returns>Повертаємо назву фотки</returns>
+        string SaveImage(string base64);
+    }
+}
